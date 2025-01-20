8d2954812ed3dfae72d2a97fe8e4b5ffe7fa97b1
Nazwha Amelia <nazwhaamelia737@gmail.com>
2025-01-20 20:10:18 +0700

    nambahin table

---
+++ database/migrations/2025_01_20_095607_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->foreignId('categori-id')->references('id')->on('categories');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('thumbnail');
+            $table->string('status');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
