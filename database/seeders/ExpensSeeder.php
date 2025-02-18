475df0c9e93be04d63ab474b701f7d09266f22f0
Nazwha Amelia <nazwhaamelia737@gmail.com>
2025-02-18 08:41:08 +0700

    fix seeder

--- database/seeders/ExpensSeeder.php
+++ database/seeders/ExpensSeeder.php
@@ -15,23 +15,16 @@ class ExpensSeeder extends Seeder
     public function run(): void
     {
         $faker = Faker::create();
+        for ($i = 0; $i < 10; $i++) {
         //
         DB::table('expenses')->insert([
             'id_greenhouse' => 1,
             'id_buy' => $faker->numberBetween(1, 3),
             'id_user' => $faker->numberBetween(1,3),
             'date' => $faker->date,
-            'total_price' => $faker->randomFloat(2,0,100),
+            'total_price' => $faker->numberBetween(2,5000),
         ]);
 
-        for ($i = 0; $i < 10; $i++) {
-            DB::table('expenses')->insert([
-                'id_greenhouse' => 1,
-                'id_buy' => $faker->numberBetween(1, 3),
-                'id_user' => $faker->numberBetween(1,3),
-                'date' => $faker->date,
-                'total_price' => $faker->numberBetween(2, 100),
-            ]);
         }
     }
 }
