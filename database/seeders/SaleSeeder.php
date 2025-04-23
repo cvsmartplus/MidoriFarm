c792c19fc252eba2a5ce4ccea0daf6cf046884e8
Nazwha Amelia <nazwhaamelia737@gmail.com>
2025-04-23 11:31:59 +0700

    migration + seed penjualan

--- database/seeders/SaleSeeder.php
+++ database/seeders/SaleSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
+use Illuminate\Support\Str;
 
 class SaleSeeder extends Seeder
 {
@@ -16,19 +17,38 @@ class SaleSeeder extends Seeder
     {
         $faker = Faker::create();
         for ($i = 0; $i < 10; $i++) {
-            $id_product = $faker->numberBetween(2, 10);
-            $price = $faker->numberBetween(50000, 200000);
-            $quantity = $faker->numberBetween(1, 100);
-        //
-        DB::table('sales')->insert([
-            'id_greenhouse' => $faker->numberBetween(1,5),
-            'id_product' => $id_product,
-            'price' => $price,
-            'quantity' => $quantity,
-            'subtotal' => $price * $quantity,
-            'date' => $faker->date(),
-        ]);
+            $transaction_id = Str::uuid();
+            $total = 0;
+            $items = [];
 
+            $product_count = rand(2, 5);
+
+            for ($i = 0; $i < $product_count; $i++) {
+                $id_product = $faker->numberBetween(1, 10);
+                $price = $faker->randomElement([25000, 30000, 35000]);
+                $quantity = $faker->numberBetween(1, 5);
+                $subtotal = $price * $quantity;
+
+                $total += $subtotal;
+
+                $items[] = [
+                    'id_greenhouse' => $faker->numberBetween(1,5),
+                    'id_product' => $id_product,
+                    'transaction_id' => $transaction_id,
+                    'price' => $price,
+                    'quantity' => $quantity,
+                    'subtotal' => $subtotal,
+                    'total' => 0,
+                    'date' => $faker->date(),
+                ];
+            }
+
+            foreach ($items as &$item) {
+                $item['total'] = $total;
+            }
+
+        //
+        DB::table('sales')->insert($items);
         }
     }
 }
