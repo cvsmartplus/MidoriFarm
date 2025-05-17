34af001cb373a578f6b46555dfeffcb0e297dc2e
Muhammad Sultan <sultanurulloh08@gmail.com>
2025-05-18 03:51:01 +0700

    delete method tidak berguna + view nya

--- app/Http/Controllers/TagihanPemasokController.php
+++ app/Http/Controllers/TagihanPemasokController.php
@@ -23,10 +23,6 @@ class TagihanPemasokController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-    //
-    }
 
     /**
      * Store a newly created resource in storage.
